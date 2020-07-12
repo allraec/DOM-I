908cff0bcbf2ec042f8ef09089dd056b4a41be14
allraec <allraec@yahoo.com>
2020-07-11 20:31:14 -0500

    task 2

--- js/index.js
+++ js/index.js
@@ -39,6 +39,58 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navlinks = document.querySelectorAll("a");
+
+navlinks[0].textContent = siteContent["nav"]["nav-item-1"];
+navlinks[1].textContent = siteContent["nav"]["nav-item-2"];
+navlinks[2].textContent = siteContent["nav"]["nav-item-3"];
+navlinks[3].textContent = siteContent["nav"]["nav-item-4"];
+navlinks[4].textContent = siteContent["nav"]["nav-item-5"];
+navlinks[5].textContent = siteContent["nav"]["nav-item-6"];
+
+let bigText = document.querySelector("h1");
+let linebreak = document.createElement("br");
+let bigTextString = siteContent["cta"]["h1"].split(" ");
+bigText.textContent = bigTextString[0]+"\n"+bigTextString[1]+"\n"+bigTextString[2];
+bigText.style = "white-space: pre";
+
+let button = document.querySelector("button");
+button.textContent = "Get Started";
+
+let ctaImg = document.querySelector("#cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let h4 = document.querySelectorAll("h4");
+
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+h4[5].textContent = siteContent["contact"]["contact-h4"];
+
+let texts = document.querySelectorAll("p");
+
+texts[0].textContent = siteContent["main-content"]["features-content"];
+texts[1].textContent = siteContent["main-content"]["about-content"];
+texts[2].textContent = siteContent["main-content"]["services-content"];
+texts[3].textContent = siteContent["main-content"]["product-content"];
+texts[4].textContent = siteContent["main-content"]["vision-content"];
+
+let address1 = siteContent["contact"]["address"].slice(0,18);
+let address2 = siteContent["contact"]["address"].slice(19);
+
+texts[5].textContent = address1 + "\n" + address2;
+texts[5].style = "white-space: pre";
 
 
+texts[6].textContent = siteContent["contact"]["phone"];
+texts[7].textContent = siteContent["contact"]["email"];
+
+let middleImg = document.querySelector("#middle-img");
+
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+texts[8].textContent = siteContent["footer"]["copyright"];
