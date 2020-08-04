62f27abea7b0a7dd16db6327e623f9677dfa255f
Allison Castaneda <allraec@yahoo.com>
2020-08-04 18:52:35 -0500

    DRYer code

--- js/index.js
+++ js/index.js
@@ -37,6 +37,90 @@ const siteContent = {
   },
 };
 
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+//                                 TASK 1                                   //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+//Sets navigation text content
+let navlinks = document.querySelectorAll("a");
+
+for(let i = 0; i<navlinks.length; i++){
+  navlinks[i].textContent = siteContent["nav"][`nav-item-${i}`];
+}
+
+
+//Sets texts for h1 element. Object split into array to accomodate white space addition.
+let bigText = document.querySelector("h1");
+let linebreak = document.createElement("br");
+let bigTextString = siteContent["cta"]["h1"].split(" ");
+bigText.textContent = bigTextString[0]+"\n"+bigTextString[1]+"\n"+bigTextString[2];
+bigText.style = "white-space: pre"; //Preserves whitespace by the browser.
+
+//Sets text content of button.
+let button = document.querySelector("button");
+button.textContent = "Get Started";
+
+//Sets image beside h1
+let ctaImg = document.querySelector("#cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+//Sets h4 content of the main section.
+let h4 = document.querySelectorAll("h4");
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+h4[5].textContent = siteContent["contact"]["contact-h4"];
+
+//Sets middle image in main section.
+let middleImg = document.querySelector("#middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+//Sets block text content of main section.
+let texts = document.querySelectorAll("p");
+texts[0].textContent = siteContent["main-content"]["features-content"];
+texts[1].textContent = siteContent["main-content"]["about-content"];
+texts[2].textContent = siteContent["main-content"]["services-content"];
+texts[3].textContent = siteContent["main-content"]["product-content"];
+texts[4].textContent = siteContent["main-content"]["vision-content"];
+
+//Sets text context of address of contact section.
+let address1 = siteContent["contact"]["address"].slice(0,18);
+let address2 = siteContent["contact"]["address"].slice(19);
+texts[5].textContent = address1 + "\n" + address2;
+texts[5].style = "white-space: pre";
+
+//Sets phone # and email text context and copyright section.
+texts[6].textContent = siteContent["contact"]["phone"];
+texts[7].textContent = siteContent["contact"]["email"];
+texts[8].textContent = siteContent["footer"]["copyright"];
+
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+//                                 TASK 2                                   //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
+//Sets navigation texts color to green.
+
+for(let i = 0; i<navlinks.length; i++){
+  navlinks[i].style.color = 'green';
+}
+
+//Adds two more elements to the navbar called 'Home' and 'FAQs' at the front and end of the nav bar.
+let navbar = document.querySelector("nav");
+let newlink1 = document.createElement("a");
+newlink1.textContent = "Home";
+newlink1.style.color = "green";
+let newlink2 = document.createElement("a");
+newlink2.textContent = "FAQs";
+newlink2.style.color = "green";
+navbar.prepend(newlink1);
+navbar.appendChild(newlink2);
